f9496afe8bbeb5627565bb5031756f08cf191d5a
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-06-30 23:04:55 +0100

    update

--- stage 1/index.js
+++ stage 1/index.js
@@ -13,34 +13,9 @@ app.set('trust proxy', true)
 
 app.use('/api', apiRoutes);
 
-app.use((req, res, next) => {
-    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    req.clientIp = clientIp;
-    next();
-  });
-  
-  app.get('/get-location', async (req, res) => {
-    sendResponse(req, res);
-    console.log(req.ip);
-    const ip = req.clientIp;
-    console.log(`Client IP: ${ip}`);
-    
-    // Replace with your IPinfo API key
-    const apiKey = 'YOUR_IPINFO_API_KEY';
-    const url = `https://ipinfo.io/${ip}?token=${apiKey}`;
-  
-    try {
-      const response = await axios.get(url);
-      const location = response.data;
-      res.json({
-        ip,
-        location
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error retrieving location' });
-    }
-  });
+app.get('/', (req, res) => {
+    res.send("Hello");
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on localhost:${process.env.PORT}`);
